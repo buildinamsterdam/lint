699ef6fdbf02e66af49413ebfa07ce45989935bc
Paulo Ferreira Jorge <mfjpaulo@gmail.com>
2023-01-17 14:08:50 +0000

    Tweak

--- rules/react.js
+++ rules/react.js
@@ -1,8 +1,7 @@
 /**
  * **Note**: As 'react' isn't installed locally, when running 'eslint' it warns;
- * "React version not specified in eslint-plugin-react settings.". You can
- * safely ignore this warning as when this package is installed this won't be
- * an issue.
+ * "React version not specified in eslint-plugin-react settings.". This won't be
+ * an issue when this package is installed, so you can safely ignore it.
  */
 module.exports = {
   // For more info: https://github.com/jsx-eslint/eslint-plugin-react#recommended
