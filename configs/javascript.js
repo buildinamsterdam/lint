a524a1e26261acbd4856ddba6646edfb31dec718
Paulo Ferreira Jorge <mfjpaulo@gmail.com>
2023-01-18 12:16:12 +0000

    Add doc

--- configs/javascript.js
+++ configs/javascript.js
@@ -1,6 +1,7 @@
 module.exports = {
   rules: {
-    // Prevent unused variables
+    // Prevent unused variables. Note that this rule can't be used in TypeScript
+    // because it errors on type definitions
     "no-unused-vars": "error",
   },
 };
